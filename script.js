c2d2d8810101c47e0e354c6850934308e1038fef
Andrew Napitupulu <angularsprings403@gmail.com>
2023-08-15 12:29:10 +0700

    finish challange fundamental 2

--- script.js
+++ script.js
@@ -11,7 +11,6 @@
 
 // console.log(firstName + lastName);
 
-
 // Boolean
 // let javascriptIsFun = true;
 // console.log(javascriptIsFun);
@@ -22,7 +21,6 @@
 // const ageAndrew  = 2023 - 1997;
 // const ageAra     = 2023 - 2003;
 // console.log(ageAndrew, ageAra);
-
 
 // let x = 10 + 5;
 // x +=10;
@@ -77,3 +75,43 @@
 // console.log(`Lines 1 
 // Lines 2 
 // Lines 3`);
+
+// const age = 10;
+
+// if(age >= 16)
+// {
+//     console.log("Andrew Can Starts Driving License");
+// }else{
+//     const yearsOld = 16 - age;
+//     console.log(`Andrew is too young ,wait another ${yearsOld} years old :D`)
+// }
+
+// const birthdayYear = 1998;
+
+// let century;
+
+// if(birthdayYear <= 2000)
+// {
+//     century = 20
+// }else{
+//     century = 21
+// }
+// console.log(century);
+
+// Challange Fundamental 2
+
+const weightMark = 95;
+const heightMark = 1.88;
+const weightJohn = 85;
+const heightJohn = 1.76;
+
+const BMIMark = weightMark / heightMark ** 2;
+const BMIJohn = weightJohn / (heightJohn * 2);
+console.log(BMIMark, BMIJohn);
+
+if(BMIMark > BMIJohn)
+{
+    console.log(`Mark's BMI is (${BMIMark}) higher than john's (${BMIJohn})!`);
+}else{
+    console.log(`John's BMI (${BMIJohn}) is higher than mark's (${BMIMark})!`);
+}
