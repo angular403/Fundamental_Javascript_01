e3aca963488c03efed575ee4330ce015be2b6f95
Andrew Napitupulu <angularsprings403@gmail.com>
2023-08-15 09:50:09 +0700

    Latihan Javascript Dasar

---
+++ script.js
@@ -0,0 +1,10 @@
+let angka = 50 + 30 - 20 / 2
+console.log(angka)
+
+console.log('andrew');
+console.log(40);
+
+let firstName = "Andrew ";
+let lastName = "Wiliam";
+
+console.log(firstName + lastName);
