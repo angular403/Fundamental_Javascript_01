9b07319d03acc8973ef277f04ba2c7e921c52399
Andrew Napitupulu <angularsprings403@gmail.com>
2023-08-15 11:20:15 +0700

    Math Operation

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
-let angka = 50 + 30 - 20 / 2
-console.log(angka)
+// Variable
 
-console.log('andrew');
-console.log(40);
+// let angka = 50 + 30 - 20 / 2
+// console.log(angka)
 
-let firstName = "Andrew ";
-let lastName = "Wiliam";
+// console.log('andrew');
+// console.log(40);
 
-console.log(firstName + lastName);
+// let firstName = "Andrew ";
+// let lastName = "Wiliam";
+
+// console.log(firstName + lastName);
+
+
+// Boolean
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 24);
+// console.log(typeof 'Andrew');
+
+// const ageAndrew = 2023 - 1997;
+// const ageAra = 2023 - 2003;
+// console.log(ageAndrew, ageAra);
+
+
+// let x = 10 + 5;
+// x +=10;
+// x *=5;
+// console.log(x);
+
+// console.log(ageAndrew > ageAra);
+// console.log(ageAndrew <= ageAra);
+
+// let x,y;
+// x = y = 25 -10 -5;
+// console.log(x,y);
+
+// const averageAge = ageAndrew + ageAra / 2;
+// console.log(ageAndrew, ageAra, averageAge);
+
